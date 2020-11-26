56a53cfbb9710423c450d39991b23fcbe5b3fb8f
Alex Vong <avong2011@gmail.com>
2020-11-26 07:03:40 -0500

    feat: styling hero section

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,42 @@
 import './scss/app.scss';
-import Axios from 'axios';
-import accordion from './accordion';
-import slider from './slider';
+// import Axios from 'axios';
+// import accordion from './accordion';
+// import slider from './slider';
 import scrollTrigger from './scroll-trigger';
 
-accordion.initAccordion();
-slider.initSlider();
+// accordion.initAccordion();
+// slider.initSlider();
 scrollTrigger.initScrollTrigger();
 
-async function getData() {
-  const masterRef = await Axios.get(`${process.env.API_URL}`).then((resp) => {
-    return resp.data.refs[0].ref;
-  });
+// async function getData() {
+//   const masterRef = await Axios.get(`${process.env.API_URL}`).then((resp) => {
+//     return resp.data.refs[0].ref;
+//   });
 
-  const dataRef = Axios.get(`${process.env.API_URL}/documents/search`, {
-    params: {ref: masterRef, access_token: process.env.ACCESS_TOKEN},
-  }).then((resp) => {
-    return resp.data.results;
-  });
-  return dataRef;
-}
-async function returnData() {
-  const data = await getData();
-  accessData(data);
-}
-returnData();
+//   const dataRef = Axios.get(`${process.env.API_URL}/documents/search`, {
+//     params: {ref: masterRef, access_token: process.env.ACCESS_TOKEN},
+//   }).then((resp) => {
+//     return resp.data.results;
+//   });
+//   return dataRef;
+// }
+// async function returnData() {
+//   const data = await getData();
+//   accessData(data);
+// }
+// returnData();
 
-function accessData(data: any) {
-  const title = document.createElement('h1');
-  const img = document.createElement('img');
-  // Lodash, currently included via a script, is required for this line to work
-  title.innerHTML = data[0].data.title[0].text;
-  document.body.appendChild(title);
+// function accessData(data: any) {
+//   const title = document.createElement('h1');
+//   const img = document.createElement('img');
+//   // Lodash, currently included via a script, is required for this line to work
+//   title.innerHTML = data[0].data.title[0].text;
+//   document.body.appendChild(title);
 
-  const copy = document.createElement('p');
-  copy.innerHTML = data[0].data.description[0].text;
-  document.body.appendChild(copy);
+//   const copy = document.createElement('p');
+//   copy.innerHTML = data[0].data.description[0].text;
+//   document.body.appendChild(copy);
 
-  img.setAttribute('src', data[0].data.hero_banner.url);
-  document.body.appendChild(img);
-}
+//   img.setAttribute('src', data[0].data.hero_banner.url);
+//   document.body.appendChild(img);
+// }
