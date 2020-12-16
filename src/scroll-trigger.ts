b36e8f0114c970ed3529ae2593d34f1afa49433a
Alex Vong <avong2011@gmail.com>
2020-12-15 19:18:40 -0500

    feat: added workouts content

--- src/scroll-trigger.ts
+++ src/scroll-trigger.ts
@@ -8,6 +8,7 @@ const scrollTrigger = {
     this.heroSection();
     this.introSection();
     this.metricsSection();
+    this.workoutsSection();
   },
   hideVideo: function (ev: any) {
     ev.target.removeEventListener('ended', this.hideVideo);
@@ -542,6 +543,60 @@ const scrollTrigger = {
         },
         0
       );
+  },
+  workoutsSection: function () {
+    const workoutsHero = gsap.timeline({
+      scrollTrigger: {
+        markers: false,
+        trigger: '[data-trigger="workouts-hero"]',
+        start: '-=100 center',
+        end: 'bottom center',
+        scrub: 0.75,
+        onUpdate: (self) => {
+          const progress = Math.floor(self.progress * 100);
+          const heroReveal = document.querySelector<HTMLElement>('[data-target="workouts-hero"]');
+          if (heroReveal) {
+            let endProgress = progress * 6;
+            endProgress > 100 ? (endProgress = 100) : endProgress;
+            heroReveal.style.setProperty('--progress-start', `${progress}%`);
+            heroReveal.style.setProperty('--progress-end', `${endProgress}%`);
+          }
+        },
+      },
+    });
+
+    workoutsHero
+      .to('[data-target="workouts-metric-music"]', {
+        yPercent: -160,
+      })
+      .from(
+        '[data-target="workouts-metric-clock"]',
+        {
+          yPercent: 20,
+        },
+        0
+      )
+      .to(
+        '[data-target="workouts-metric-yoga"]',
+        {
+          yPercent: 180,
+        },
+        0
+      );
+
+    gsap.from('[data-target="workouts-intro"]', {
+      stagger: 0.25,
+      ease: 'none',
+      opacity: 0,
+      y: 50,
+      scrollTrigger: {
+        markers: false,
+        trigger: '[data-trigger="workouts-content"]',
+        scrub: 0.45,
+        start: '-=400 center',
+        end: 'center center',
+      },
+    });
   },
 };
 
